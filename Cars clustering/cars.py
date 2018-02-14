16246d35f367955b84c7e5923e115b830bcc84c1
TheBrownViking20 <siddharth16csu361@ncuindia.edu>
2018-02-15 00:06:06 +0530

    Cars k-means clustrering

---
+++ Cars clustering/cars.py
@@ -0,0 +1,54 @@
+# K-Means Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('cars.csv')
+
+X = dataset.iloc[:,:-1].values
+
+X = pd.DataFrame(X)
+X = X.convert_objects(convert_numeric=True)
+X.columns = ['mpg', ' cylinders', ' cubicinches', ' hp', ' weightlbs', ' time-to-60', 'year']
+
+X = pd.DataFrame(X)
+X = X.convert_objects(convert_numeric=True)
+X.columns = ['mpg', ' cylinders', ' cubicinches', ' hp', ' weightlbs', ' time-to-60', 'year']
+
+# Eliminating null values
+for i in X.columns:
+    X[i] = X[i].fillna(int(X[i].mean()))
+for i in X.columns:
+    print(X[i].isnull().sum())
+    
+
+# Using the elbow method to find  the optimal number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i,init='k-means++',max_iter=300,n_init=10,random_state=0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1,11),wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# Applying k-means to the cars dataset
+kmeans = KMeans(n_clusters=3,init='k-means++',max_iter=300,n_init=10,random_state=0) 
+y_kmeans = kmeans.fit_predict(X)
+
+X = X.as_matrix(columns=None)
+
+# Visualising the clusters
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0,1],s=100,c='red',label='US')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1,1],s=100,c='blue',label='Japan')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2,1],s=100,c='green',label='Europe')
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=300,c='yellow',label='Centroids')
+plt.title('Clusters of car brands')
+plt.legend()
+plt.show()
