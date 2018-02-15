2f3355d86c01355b376a9b4e8342f1d0736c553a
TheBrownViking20 <siddharth16csu361@ncuindia.edu>
2018-02-15 23:16:16 +0530

    pokemon classification improved

--- Pokemon/pokemon_legendary_classification.py
+++ Pokemon/pokemon_legendary_classification.py
@@ -1,6 +1,6 @@
 # Importing Libraries
 import numpy as np
-import pandas as 
+import pandas as pd
 import matplotlib.pyplot as plt
 
 # Importing Data
@@ -13,11 +13,16 @@ y = dataset.iloc[:,12].values
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
+a_name = ['Logistic Regression','KNN','SVM','Naive Bayes','Decision Trees','Random Forest']
+wrong_pred_list = []
+
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# In the following algorithms, we will use confusion matrix which determines the count of right and wrong predictions. The elements of main or principal diagonal represent the number of correct predictions and the other diagonal represents incorrect predictions  
 
 # -----+++++Logistic Regression+++++-----
 
@@ -33,6 +38,8 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
 
+wrong_pred_list.append(cm)
+
 # -----+++++K-Nearest Neighbors (K-NN)+++++-----
 
 # Fitting K-NN to the Training set
@@ -46,6 +53,8 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+wrong_pred_list.append(cm)
 
 # -----+++++Support Vector Machine+++++-----
 
@@ -62,6 +71,8 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
+wrong_pred_list.append(cm)
+
 # -----+++++Naive Bayes+++++-----
 
 # Fitting classifier to the Training set
@@ -71,6 +82,8 @@ classifier.fit(X_train,y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
+
+wrong_pred_list.append(cm)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
@@ -90,6 +103,8 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
+wrong_pred_list.append(cm)
+
 # -----+++++Random Forest+++++-----
 
 # Fitting classifier to the Training set
@@ -103,3 +118,19 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+wrong_pred_list.append(cm)
+
+# -----+++++Choosing the best algoruithm+++++-----
+
+output_data = pd.DataFrame({'Algorithm': a_name, 'Confusion Matrix': wrong_pred_list})
+
+wrong_pred_sum = []
+for i in output_data['Confusion Matrix']:
+    wrong_pred_sum.append(i[0][1] + i[1][0])
+    
+output_data['Wrong Predictions'] = wrong_pred_sum
+
+print(output_data)
+print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("As Logistic Regression, Support Vector Machine and Naive Bayes Algorithms have least number of errors i.e., 7 so they are the best for classifying legendary pokemon")
