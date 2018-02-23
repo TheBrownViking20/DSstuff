4ed5a396b8c99df2a7183260f962fb89f588884d
TheBrownViking20 <siddharth16csu361@ncuindia.edu>
2018-02-23 16:20:28 +0530

    video game sales

---
+++ Kickstarter/kickstarter.py
@@ -0,0 +1,9 @@
+# Importing Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+
+# Importing data
+dataset = pd.read_csv(u'ks-projects-201612.csv',encoding='cp1252')
