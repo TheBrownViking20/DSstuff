1a9efd50d7266301955351c66c3c9be1e1c10f01
TheBrownViking20 <siddharth16csu361@ncuindia.edu>
2018-04-05 12:25:40 +0530

    seinfeld v3

--- seinfeld/seinfeld.py
+++ seinfeld/seinfeld.py
@@ -1,10 +1,6 @@
 # Importing libraries
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-%matplotlib inline
-
 
 # importing data
 df = pd.read_csv("scripts.csv")
@@ -12,9 +8,9 @@ del df["Unnamed: 0"]
 
 dial_df = df.drop(["EpisodeNo","SEID","Season"],axis=1)
 
-dial_df = dial_df[(dial_df["Character"]=="JERRY") | (dial_df["Character"]=="ELAINE") | (dial_df["Character"]=="GEORGE") | (dial_df["Character"]=="KRAMER")]
+dial_df = dial_df[(dial_df["Character"]=="ELAINE") | (dial_df["Character"]=="GEORGE") | (dial_df["Character"]=="KRAMER")]
 
-dial_df["Dialogue"][0]
+dial_df["Dialogue"][4]
 
 from sklearn.feature_extraction import text
 punc = ['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}',"%"]
@@ -28,30 +24,37 @@ def text_process(dialogue):
 X = dial_df["Dialogue"]
 y = dial_df["Character"]
 
-from sklearn.feature_extraction.text import CountVectorizer
-vectorizer = CountVectorizer(analyzer=text_process).fit(X)
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(analyzer=text_process).fit(X)
 
 print(len(vectorizer.vocabulary_))
-
 X = vectorizer.transform(X)
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=101)
 
-from sklearn.linear_model import LogisticRegression as RFC
-rfc = RFC()
-rfc.fit(X_train, y_train)
+from sklearn.naive_bayes import MultinomialNB as MNB
+from sklearn.linear_model import LogisticRegression as LR
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.ensemble import VotingClassifier as VC
+mnb = MNB(alpha=10)
+lr = LR(random_state=101)
+rfc = RFC(n_estimators=80, criterion="entropy", random_state=42, n_jobs=-1)
+clf = VC(estimators=[('mnb', mnb), ('lr', lr), ('rfc', rfc)], voting='hard')
 
-predict = rfc.predict(X_test)
+clf.fit(X_train,y_train)
+
+predict = clf.predict(X_test)
 
 from sklearn.metrics import confusion_matrix, classification_report
 print(confusion_matrix(y_test, predict))
 print('\n')
 print(classification_report(y_test, predict))
 
+
 def predictor(s):
     s = vectorizer.transform(s)
-    pre = rfc.predict(s)
+    pre = clf.predict(s)
     print(pre)
 
-predictor(['One night with me can make any woman lesbian.'])
+predictor(['I\'m on the Mexican, whoa oh oh, radio.'])
